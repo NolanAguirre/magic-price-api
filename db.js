4731e1d639aac80a08f705791d137fcbf847464b
nolan <nolanaguirre08@gmail.com>
2018-01-03 23:31:34 -0600

    add basic database functions

--- db.js
+++ db.js
@@ -3,3 +3,24 @@ var pgp = require('pg-promise')( /*options*/ )
 pgp.pg.defaults.ssl = true;
 var db = {};
 var database = pgp(process.env.DATABASE_URL);
+
+db.updatePrice = function(queryParams, price){
+  db.none("UPDATE magic_cards SET price = $1 WHERE multiversid = $2", [queryParams.multiversid, price])
+    .then(function(){
+
+    }).catch(function(err){
+      console.log(err)
+    })
+}
+
+db.queryCardPrice = function(queryParams, callback){
+  database.one('SELECT * FROM magic_cards WHERE name = $1', queryParams.name)
+    .then(function(data) {
+      callback(data);
+    })
+    .catch(function(err) {
+      callback(null);
+    });
+}
+
+module.export = db;
