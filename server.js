456d1c0f45693ffbff2844d5a6f5cff2467599ee
nolan <nolanaguirre08@gmail.com>
2018-01-15 18:31:35 -0600

    change tabs from 2 to 4 spaces

--- server.js
+++ server.js
@@ -9,54 +9,54 @@ const fero = require('fero');
 const oneDay = 86400000;
 
 function requestHandler(
-  cards
+    cards
 ) {
-  return function(req, res) {
-    cards.on('change')
-      .filter(function(msg) {
-        return msg._key == req.url && msg.value.price;
-      }).map(function(msg) {
-        if (!res._headerSent) {
-          res.send(msg.value)
+    return function(req, res) {
+        cards.on('change')
+            .filter(function(msg) {
+                return msg._key == req.url && msg.value.price;
+            }).map(function(msg) {
+                if (!res._headerSent) {
+                    res.send(msg.value)
+                }
+            })
+        let urlKey = req.url;
+        console.log("got a get request ", urlKey);
+        if (cards[urlKey] && Date.now() - cards[urlKey].date < oneDay) { // has been scraped today
+            res.send(cards[urlKey]);
+        } else if (!cards[urlKey]) { // has never been scraped
+            cards.add(urlKey, {
+                type: 'SCRAPE',
+                value: {
+                    price: null,
+                    url: urlKey,
+                    date: null
+                }
+            });
+        } else { // has been scraped, but is older than one day
+            cards.update(urlKey, {
+                type: 'SCRAPE',
+                value: {
+                    price: null,
+                    url: urlKey,
+                    date: null
+                }
+            })
         }
-      })
-    let urlKey = req.url;
-    console.log("got a get request ", urlKey);
-    if (cards[urlKey] && Date.now() - cards[urlKey].date < oneDay) { // has been scraped today
-      res.send(cards[urlKey]);
-    } else if (!cards[urlKey]) { // has never been scraped
-      cards.add(urlKey, {
-        type: 'SCRAPE',
-        value: {
-          price: null,
-          url: urlKey,
-          date: null
-        }
-      });
-    } else { // has been scraped, but is older than one day
-      cards.update(urlKey, {
-        type: 'SCRAPE',
-        value: {
-          price: null,
-          url: urlKey,
-          date: null
-        }
-      })
     }
-  }
 }
 
 async function createApp() {
-  const cards = await fero('cards', {
-    client: true
-  })
-  await cards.on('connected');
-  const app = express();
-  const localRequestHandler = requestHandler(cards)
-  app.use(cors());
-  app.get('/*', localRequestHandler)
-  return app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
+    const cards = await fero('cards', {
+        client: true
+    })
+    await cards.on('connected');
+    const app = express();
+    const localRequestHandler = requestHandler(cards)
+    app.use(cors());
+    app.get('/*', localRequestHandler)
+    return app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+    });
 }
 createApp();
--- worker.js
+++ worker.js
@@ -3,37 +3,37 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const scrape = function(req, cache) {
-    let price;
-    let url = req.value.value.url;
-    request('https://www.mtggoldfish.com/price' + url + '#paper', function(error, response, html) {
-      if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        try {
-          price = $('div[class=price-box-price]').get(1)['children'][0]['data'];
-        } catch (err) {
-          price = $('div[class=price-box-price]').text();
-        }
-        cache.update(url, {
-          price: price,
-          date: Date.now()
-        })
-      } else {
-        cache.update(url, {
-          price: "cannot be fetched at this time",
-          date: 0
-        })
-      }
-    });
-  }
+        let price;
+        let url = req.value.value.url;
+        request('https://www.mtggoldfish.com/price' + url + '#paper', function(error, response, html) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(html);
+                try {
+                    price = $('div[class=price-box-price]').get(1)['children'][0]['data'];
+                } catch (err) {
+                    price = $('div[class=price-box-price]').text();
+                }
+                cache.update(url, {
+                    price: price,
+                    date: Date.now()
+                })
+            } else {
+                cache.update(url, {
+                    price: "cannot be fetched at this time",
+                    date: 0
+                })
+            }
+        });
+    }
 
-  !async function start() {
-    const cards = await fero('cards', async function(req, cache) {
-      switch (req.value.type) {
-        case "SCRAPE":
-          return scrape(req, cache);
-        default:
-          return "Method Not Allowed";
-      }
-    })
-    await cards.on('connected');
-  }()
+    !async function start() {
+        const cards = await fero('cards', async function(req, cache) {
+            switch (req.value.type) {
+                case "SCRAPE":
+                    return scrape(req, cache);
+                default:
+                    return "Method Not Allowed";
+            }
+        })
+        await cards.on('connected');
+    }()
